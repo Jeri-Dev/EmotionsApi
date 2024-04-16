648641bd015fd10894544509fbe03ba1f1aad93f
Jeriel <jgomezinf19@gmail.com>
2024-04-16 17:49:57 -0400

    add cors

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 from fastapi import FastAPI, File, UploadFile
 import emotionanalyzer
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-@app.post("/upload-audio")
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/upload-audio")
 async def upload_file(file: UploadFile = File(...)):
     contenido_archivo = await file.read()
     text, emotion = emotionanalyzer.getAnalysis(contenido_archivo) 
@@ -17,6 +26,6 @@ async def upload_file(file: UploadFile = File(...)):
 def root():
     return {"messagge" : "Welcome to EmotionsAPI"}
 
-@app.post("/getText")
+@app.get("/getText")
 def getText(text: str):
     return {"El texto introducido fue:" : text}
